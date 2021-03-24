7e8fb5d81adb7f12c7720f4e45791668905573f9
Hargi <hargimuhammad@gmail.com>
2021-03-24 16:43:10 +0700

    still error

---
+++ Copying a File/ScannerW2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studikasus3;
+
+import java.io.File;
+import java.util.Scanner;
+public class ScannerW2
+{
+  public static void main(String[] args) throws Exception
+  {
+    Scanner sc = new Scanner(file);
+  
+    while (sc.hasNextLine())
+      System.out.println(sc.nextLine());
+  }
+}
