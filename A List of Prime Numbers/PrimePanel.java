155cbc8e5de885109a801361412ed5186e015da4
Hargi <hargimuhammad@gmail.com>
2021-03-25 21:51:59 +0700

    finish

--- A List of Prime Numbers/PrimePanel.java
+++ A List of Prime Numbers/PrimePanel.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package studikasus5;
-
-/**
- *
- * @author Hargi
- */
 // ****************************************************************
 // PrimePanel.java
 //
