9d21a890b0daaf7b5359ff3c86cd2152c5f88091
Hargi <hargimuhammad@gmail.com>
2021-03-25 20:46:21 +0700

    almost fix

--- Enhancing a Movable Circle/CirclePanel.java
+++ Enhancing a Movable Circle/CirclePanel.java
@@ -19,6 +19,10 @@ public class CirclePanel extends JPanel
 private final int CIRCLE_SIZE = 50;
 private int x,y;
 private Color c;
+JButton left;
+JButton right;
+JButton up;
+JButton down;
 //---------------------------------------------------------------
 // Set up circle and buttons to move it.
 //---------------------------------------------------------------
@@ -31,15 +35,25 @@ c = Color.green;
 // Need a border layout to get the buttons on the bottom
 this.setLayout(new BorderLayout());
 // Create buttons to move the circle
-JButton left = new JButton("Left");
-JButton right = new JButton("Right");
-JButton up = new JButton("Up");
-JButton down = new JButton("Down");
+left = new JButton("Left");
+right = new JButton("Right");
+up = new JButton("Up");
+down = new JButton("Down");
+left.setToolTipText("To Left 20 coordinat");
+right.setToolTipText("To Right 20 coordinat");
+up.setToolTipText("To Up 20 coordinat");
+down.setToolTipText("To Down 20 coordinat");
+//left.setEnable(x < 0);
 // Add listeners to the buttons
 left.addActionListener(new MoveListener(-20,0));
 right.addActionListener(new MoveListener(20,0));
 up.addActionListener(new MoveListener(0,-20));
 down.addActionListener(new MoveListener(0,20));
+left.setMnemonic('l');
+right.setMnemonic('r');
+up.setMnemonic('u');
+down.setMnemonic('d');
+
 // Need a panel to put the buttons on or they'll be on
 // top of each other.
 JPanel buttonPanel = new JPanel();
@@ -66,13 +80,17 @@ private class MoveListener implements ActionListener
 {
 private int dx;
 private int dy;
+
 //---------------------------------------------------------------
 // Parameters tell how to move circle at click.
 //---------------------------------------------------------------
 public MoveListener(int dx, int dy)
 {
+    
 this.dx = dx;
+
 this.dy = dy;
+
 }
 //---------------------------------------------------------------
 // Change x and y coordinates and repaint.
@@ -80,8 +98,14 @@ this.dy = dy;
 public void actionPerformed(ActionEvent e)
 {
 x += dx;
+
 y += dy;
+left.setEnabled(x>0);
+right.setEnabled(x<330);
+up.setEnabled(y>0);
+down.setEnabled(y<130);
+
 repaint();
 }
 }
-}
+}   
